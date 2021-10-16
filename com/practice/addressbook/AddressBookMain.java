91b21481cd3bde3c1dc4947c21b7f1a9d5c7a21b
Shivammishra789 <shivammishra7789@gmail.com>
2021-10-16 15:08:10 +0530

    creating new contact

--- com/practice/addressbook/AddressBookMain.java
+++ com/practice/addressbook/AddressBookMain.java
@@ -4,9 +4,11 @@ public class AddressBookMain {
 
 	public static void main(String[] args) {
 
-		ContactList person1 = new ContactList("Shivam","Mishra","Lane no 24","Pune","Maharashtra",
-				"411039","8830466378","Shivammishra7789@gmail.com");
+		ContactList person1 = new ContactList("Shivam", "Mishra", "Lane no 24", "Pune", "Maharashtra",
+				"411039", "8830466378", "Shivammishra7789@gmail.com");
+		ContactList person2 = new ContactList("Rahul", "Krishna", "No.2,AK street, 3rd Cross", "XXY city", "MMN state",
+				"660666", "9194979295", "rahulkrisna@gmail.com");
 		System.out.println(person1.toString());
-
+		System.out.println(person2.toString());
 	}
 }
