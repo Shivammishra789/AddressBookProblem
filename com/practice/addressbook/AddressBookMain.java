4623af27b2e68dcdfa244b1b64ef02a338e1db92
Shivammishra789 <shivammishra7789@gmail.com>
2021-10-19 21:44:34 +0530

    editing existing contact by console

--- com/practice/addressbook/AddressBookMain.java
+++ com/practice/addressbook/AddressBookMain.java
@@ -5,24 +5,23 @@ import java.util.Scanner;
 
 public class AddressBookMain {
 
+	Scanner sc = new Scanner(System.in);
+
+	//creating arraylist to create store contact
+	ArrayList<ContactList> contactData = new  ArrayList<ContactList>();
+
 	public static void main(String[] args) {
-		addContact();										//calling method in main
+		AddressBookMain object = new AddressBookMain();
+		object.addContact();
 	}
 
-	public static void addContact() {
-
-		Scanner sc = new Scanner(System.in);
-
-		//creating arraylist to create store contact
-		ArrayList<ContactList> contactData = new  ArrayList<ContactList>();
-
+	public void addContact() {
 		System.out.println("enter 1 to add new contact");
 		int startAdd = sc.nextInt();
-
 		if(startAdd == 1) {
 			while(startAdd == 1) {
 				ContactList person = new ContactList();
-				contactData.add(person);					//adding contact to arraylist
+				contactData.add(person);							//adding contact to arraylist
 				System.out.println("enter 1 to add more contact or enter 0 to exit");
 				startAdd = sc.nextInt();
 				if(startAdd == 0) {
@@ -36,10 +35,38 @@ public class AddressBookMain {
 			System.out.println("enter valid input");
 			addContact();
 		}
+		System.out.println("to edit existing contact press 5 ");
+		int edit = sc.nextInt();
+		if(edit == 5) {
+			editContact();
+		}
 
 		//displaying contact list
 		for(int i = 0; i < contactData.size(); i++) {
 			System.out.println(contactData.get(i));
+		}
+	}
+	public void editContact() {
+		System.out.println("enter first name to edit that contact");
+		Scanner sc = new Scanner(System.in);
+		String editName = sc.nextLine();
+		for(int j = 0 ; j <= contactData.size() ; j++) {
+			if(editName.equalsIgnoreCase(contactData.get(j).getFirstName())) {
+				ContactList newperson = new ContactList();
+				contactData.set(j,newperson);							//editing contact in arraylist
+				System.out.println("contact updated");
+				System.out.println();
+				System.out.println("to edit any other contact enter 5 or enter other number to exit");
+				int edit = sc.nextInt();
+				if(edit == 5) {
+					editContact();
+					break;
+				}
+				break;
+			}
+			System.out.println("entered name not found");
+			break;
+
 		}
 	}
 }
--- com/practice/addressbook/ContactList.java
+++ com/practice/addressbook/ContactList.java
@@ -8,6 +8,7 @@ public class ContactList {
 
 	//variables
 	private String firstName, lastName, address, city, state, zip, phoneNo, emailId;
+	
 
 	public ContactList() {
 
@@ -36,6 +37,72 @@ public class ContactList {
 		System.out.println("enter email Id");
 		emailId = sc.nextLine();
 	}
+
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getZip() {
+		return zip;
+	}
+
+	public void setZip(String zip) {
+		this.zip = zip;
+	}
+
+	public String getPhoneNo() {
+		return phoneNo;
+	}
+
+	public void setPhoneNo(String phoneNo) {
+		this.phoneNo = phoneNo;
+	}
+
+	public String getEmailId() {
+		return emailId;
+	}
+
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+
 
 	@Override
 	public String toString() {
